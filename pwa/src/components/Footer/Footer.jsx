570d3b754730c8fbdb77cef8306e80943b392c2c
Huong Pham <huong.pq12@gmail.com>
2019-03-01 12:40:20 +0200

    edit footer, color variable

--- pwa/src/components/Footer/Footer.jsx
+++ pwa/src/components/Footer/Footer.jsx
@@ -7,7 +7,10 @@ class Footer extends Component {
       <footer className="footer">
         <div className="container">
           <div className="row">
-            <div className="col-sm-8">Conact us</div>
+            <div className="col-sm-8">
+              <h5 className="logo-text">A litle Hanoi</h5>
+              <p>Contact us</p>
+            </div>
             <div className="col-sm-4">Follow us on social mdeia</div>
           </div>
         </div>
